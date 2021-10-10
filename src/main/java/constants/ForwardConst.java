90d3eb6fb117552acfae6dbf7a9ea25f4ca69623
青園　拓海 <aozono.tk.97@gmail.com>
2021-10-10 13:40:49 +0900

    FrontController作成

--- src/main/java/constants/ForwardConst.java
+++ src/main/java/constants/ForwardConst.java
@@ -1,25 +1,24 @@
 package constants;
 
 /**
- *
- * リクエストパラメータの変数名、変数値、jspファイルの名前等画面遷移に関わる値を定義するEnumクラス
+ * リクエストパラメーターの変数名、変数値、jspファイルの名前等画面遷移に関わる値を定義するEnumクラス
  *
  */
 public enum ForwardConst {
 
-    // action
+    //action
     ACT("action"),
     ACT_TOP("Top"),
     ACT_EMP("Employee"),
     ACT_REP("Report"),
     ACT_AUTH("Auth"),
 
-    // command
+    //command
     CMD("command"),
     CMD_NONE(""),
     CMD_INDEX("index"),
     CMD_SHOW("show"),
-    CMD_SHOW_LOGIN("showlogin"),
+    CMD_SHOW_LOGIN("showLogin"),
     CMD_LOGIN("login"),
     CMD_LOGOUT("logout"),
     CMD_NEW("entryNew"),
@@ -28,12 +27,13 @@ public enum ForwardConst {
     CMD_UPDATE("update"),
     CMD_DESTROY("destroy"),
 
-    // jsp
+    //jsp
     FW_ERR_UNKNOWN("error/unknown"),
     FW_TOP_INDEX("topPage/index"),
     FW_LOGIN("login/login"),
     FW_EMP_INDEX("employees/index"),
     FW_EMP_SHOW("employees/show"),
+    FW_EMP_NEW("employees/new"),
     FW_EMP_EDIT("employees/edit"),
     FW_REP_INDEX("reports/index"),
     FW_REP_SHOW("reports/show"),
@@ -55,14 +55,14 @@ public enum ForwardConst {
     /**
      * 値(文字列)取得
      */
-    private String getValue() {
+    public String getValue() {
         return this.text;
     }
 
     /**
-     * 値(文字列)から、該当する定数を返却
+     * 値(文字列)から、該当する定数を返却する
      * (例: "Report"→ForwardConst.ACT_REP)
-     * @Param 値(文字列)
+     * @param 値(文字列)
      * @return ForwardConst型定数
      */
     public static ForwardConst get(String key) {
