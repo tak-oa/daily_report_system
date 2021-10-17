9ca032f9445d53b4ca8e4895f021c000a4834f0a
青園　拓海 <aozono.tk.97@gmail.com>
2021-10-17 14:46:00 +0900

    Reportモデル、ReportViewモデル作成

--- src/main/java/constants/JpaConst.java
+++ src/main/java/constants/JpaConst.java
@@ -64,6 +64,9 @@ public interface JpaConst {
     String Q_EMP_COUNT_RESISTERED_BY_CODE_DEF = "SELECT COUNT(e) FROM Employee AS e WHERE e.code = :" + JPQL_PARM_CODE;
     // 全ての日報をidの降順に取得
     String Q_REP_GET_ALL = ENTITY_REP + ".getALL";
+    String Q_REP_GET_ALL_DEF = "SELECT COUNT(r) FROM Report AS r";
+    //全ての日報の件数を取得する
+    String Q_REP_COUNT = ENTITY_REP + ".count";
     String Q_REP_COUNT_DEF = "SELECT COUNT(r) FROM Report AS r";
     // 指定した従業員が作成した日報を全件idの降順で取得
     String Q_REP_GET_ALL_MINE = ENTITY_REP + "getAllMine";
